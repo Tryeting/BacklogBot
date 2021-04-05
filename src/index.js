0a8ddcc8270194221b8463b4fc21d08f54db2215
mittsutaka <mitsutaka.murao@tryeting.jp>
2021-04-05 17:12:46 +0900

    payload表示

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+const github = require('@actions/github');
+
+const payload = JSON.stringify(github.context.payload, undefined, 2)
+console.log(`The event payload: ${payload}`);
+
 console.log("start");
