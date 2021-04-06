bd6afe4ace6e9f56a6421943a1e22cafe42d9dc8
mittsutaka <mitsutaka.murao@tryeting.jp>
2021-04-06 13:53:57 +0900

    追加

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const key = process.env.API_KEY;
 console.log(key);
 const id = process.env.ISSUE_TYPE_ID;
 console.log(id);
+const id2 = process.env.PROJECT_KEY;
+console.log(id2);
 const payload = JSON.stringify(github.context.payload, undefined, 2)
 console.log(`The event payload: ${payload}`);
 
