152bee52e390b2d7a822153034f9428fa46a00a1
mittsutaka <mitsutaka.murao@tryeting.jp>
2021-04-06 14:27:42 +0900

    非同期処理にした

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 const github = require('@actions/github');
 const client = require('request');
 
-const get = async (url,key) => {
+const get = async (url, key) => {
     return await client.get({
-        url:url,
-        qs:{
-            apiKey:key
+        url: url,
+        qs: {
+            apiKey: key
         }
     })
 }
 
-// const payload = JSON.stringify(github.context.payload, undefined, 2)
-// console.log(`The event payload: ${payload}`);
+const main = async () => {
+    console.log("start");
 
-console.log("start");
+    //環境変数の取得
+    const API_KEY = process.env.API_KEY;
+    const ISSUE_TYPE_ID = process.env.ISSUE_TYPE_ID;
+    const PROJECT_ID = process.env.PROJECT_ID;
+    const API_HOST = process.env.API_HOST;
 
-//環境変数の取得
-const API_KEY = process.env.API_KEY;
-const ISSUE_TYPE_ID = process.env.ISSUE_TYPE_ID;
-const PROJECT_ID = process.env.PROJECT_ID;
-const API_HOST = process.env.API_HOST;
+    //とりあえずBACKLOGAPIを叩いてみる
+    const url = `https://${API_HOST}/api/v2/projects/${PROJECT_ID}/issueTypes`;
+    const a = await get(url, API_KEY);
 
-//とりあえずBACKLOGAPIを叩いてみる
-const url = `https://${API_HOST}/api/v2/projects/${PROJECT_ID}/issueTypes`;
-const a = get(url,API_KEY);
+    console.log(a);
 
-console.log(a);
+}
+
+main();
+
