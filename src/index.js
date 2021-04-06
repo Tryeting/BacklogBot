c631ed3ca954016b60cb22b01f69d8359f0bc2f1
mittsutaka <mitsutaka.murao@tryeting.jp>
2021-04-06 13:45:28 +0900

    key取得

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const github = require('@actions/github');
 
+const key = process.env.BACKLOG_API_KEY;
+console.log(key);
 const payload = JSON.stringify(github.context.payload, undefined, 2)
 console.log(`The event payload: ${payload}`);
 
