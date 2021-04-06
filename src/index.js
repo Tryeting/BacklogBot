0d167b0923508a2455b444610029100f34279805
mittsutaka <mitsutaka.murao@tryeting.jp>
2021-04-06 14:16:49 +0900

    api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const github = require('@actions/github');
+const client = require('request');
 
-const key = process.env.API_KEY;
-console.log(key);
-const id = process.env.ISSUE_TYPE_ID;
-console.log(id);
-const id2 = process.env.PROJECT_KEY;
-console.log(id2);
 const payload = JSON.stringify(github.context.payload, undefined, 2)
 console.log(`The event payload: ${payload}`);
 
 console.log("start");
+
+//環境変数の取得
+const API_KEY = process.env.API_KEY;
+const ISSUE_TYPE_ID = process.env.ISSUE_TYPE_ID;
+const PROJECT_ID = process.env.PROJECT_ID;
+const API_HOST = process.env.API_HOST;
+
+//とりあえずBACKLOGAPIを叩いてみる
+const uel = `https://${API_HOST}/api/v2/projects/${PROJECT_ID}/issueTypes`;
+const a = get(url,API_KEY);
+
+console.log(a);
+
+const get = async (url,key) => {
+    return await client.get({
+        url:url,
+        qs:{
+            apiKey:key
+        }
+    })
+}
