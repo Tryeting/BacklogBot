ae86f9783a55a0e538ebab7d66bd459131e4ab6a
mittsutaka <mitsutaka.murao@tryeting.jp>
2021-04-06 13:48:06 +0900

    修正

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const github = require('@actions/github');
 
-const key = process.env.BACKLOG_API_KEY;
+const key = process.env.API_KEY;
 console.log(key);
+const id = process.env.ISSUE_TYPE_ID;
+console.log(id);
 const payload = JSON.stringify(github.context.payload, undefined, 2)
 console.log(`The event payload: ${payload}`);
 
